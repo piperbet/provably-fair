3538c96763e6c2a08fc6f31fcfd0385c30981fff
João Rafael <joaoraf@me.com>
2023-11-05 15:13:40 -0300

    refactor: improve clarity and efficiency of spin calculation functions in index.js

--- index.js
+++ index.js
@@ -1,48 +1,43 @@
+// Import the 'crypto' module for hashing
 const crypto = require('crypto');
 
-// You need to set these 3 values
+// Define and set the required values
 const serverSeed = '3b79a56f040e384220d5d8c17834b017e9f9da348f3bfb7cd86d4644bb61ad8d';
 const clientSeed = 'f054ef22bef543fb7bd392419bea3727ed297e9820d72335c68de209c08149c9';
 const playCount = 1611;
 
-// Here we use the seeds to calculate the spin
+// Calculate the spin using the provided seeds and playCount
 const spin = getSpin(serverSeed, clientSeed, playCount);
 console.log(`Spin: ${spin}`);
 
-
-/* Below this line are algorithmic functions which we use to calculate the Spin
-==================================================================================
-==================================================================================
-*/
-
+// Function to calculate the spin from a hash
 function getSpinFromHash(hash, maxSpin = 99999999) {
+    // Extract a portion of the hash
     const subHash = hash.slice(0, 13);
+    // Convert the extracted portion to a number
     const valueFromHash = Number.parseInt(subHash, 16);
-    
-    // Get dynamic result for this roll
-    const e = Math.pow(2, 52);
-    const result = valueFromHash / e;
+    // Calculate the spin result based on the extracted value
+    const result = valueFromHash / Math.pow(2, 52);
+    // Return the spin within the specified maximum range
     return Math.floor(result * maxSpin);
 }
 
+// Function to get the spin based on the seeds and playCount
 function getSpin(serverSeed, clientSeed, playCount) {
+    // Combine the seeds and playCount with an optional game parameter
     const seed = getCombinedSeed('BOXES', serverSeed, clientSeed, playCount);
-    console.log("seed", seed)
+    // Generate a hash from the combined seed
     const hash = crypto.createHmac('sha256', seed).digest('hex');
-    console.log("hash", hash)
-
+    // Calculate and return the spin using the hash
     return getSpinFromHash(hash);
 }
 
+// Function to combine and create a seed value
 function getCombinedSeed(game, serverSeed, clientSeed, playCount) {
-  // Add main parameters
-  const seedParameters = [serverSeed, clientSeed, playCount];
-
-  // Add game parameter if needed
-  if (game) {
-    seedParameters.unshift(game);
-  }
-
-  // Combine parameters to get seed value
-  return seedParameters.join('-')
+    // Prepare an array with the main parameters
+    const seedParameters = [serverSeed, clientSeed, playCount];
+    // Add the game parameter if needed
+    if (game) seedParameters.unshift(game);
+    // Combine the parameters into a single seed value
+    return seedParameters.join('-')
 }
